81c1f2cf6db646ae01ce6fba2a9f334c859bac5b
Paris Charalampidis <curlycode@outlook.com>
2022-12-17 23:45:51 +0200

    retrieve get call from weather api

--- API projects/weatherAPI/weather.py
+++ API projects/weatherAPI/weather.py
@@ -5,7 +5,21 @@ import requests
 import json
 
 #declare api key 
-
+response = requests.get(api_key)
 api_key = config.api_key
+url = "http://api.weatherapi.com/v1/current.json?key=api_key&q=location_input&aqi=no" 
 
+
+
+#print(response.url)
+print("Welcome into the Weather app")
+print("please search for a country ")
+
+
+def location_input():
+    location_input = input("> ")
+    response = requests.get(url)
+    response.json()
+    print (location_input)
+location_input()
 
