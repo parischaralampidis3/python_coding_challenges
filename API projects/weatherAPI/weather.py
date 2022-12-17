afd8c507ee8863e833b117e5038432c7c92b7d58
Paris Charalampidis <curlycode@outlook.com>
2022-12-18 01:08:26 +0200

    completed task - get call from weather api for input city

--- API projects/weatherAPI/weather.py
+++ API projects/weatherAPI/weather.py
@@ -1,25 +1,47 @@
 #import required modules
 #config module for providing keys
 import config
+#from bs4 import BeautifulSoup
 import requests
-import json
+
+
 
 #declare api key 
-response = requests.get(api_key)
-api_key = config.api_key
-url = "http://api.weatherapi.com/v1/current.json?key=api_key&q=location_input&aqi=no" 
+api_key = config.API_KEY
+#print(key)
+#print(base_url)
 
 
-
-#print(response.url)
 print("Welcome into the Weather app")
 print("please search for a country ")
 
 
+
+ 
 def location_input():
-    location_input = input("> ")
-    response = requests.get(url)
-    response.json()
-    print (location_input)
+   
+    city = input('Enter the name of the city.. >  ')
+    res = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no")
+    print ("Searching..\n")
+    print(res.json())
+    
+    
 location_input()
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 
