0873e24695ee758736e90ae4704c3e9790ee650c
Paris Charalampidis <curlycode@outlook.com>
2022-12-18 22:42:04 +0200

    finishing query weather functionality through input

--- API projects/weatherAPI/weather.py
+++ API projects/weatherAPI/weather.py
@@ -1,47 +1,36 @@
-#import required modules
-#config module for providing keys
+
 import config
-#from bs4 import BeautifulSoup
+import json
 import requests
 
 
-
-#declare api key 
 api_key = config.API_KEY
-#print(key)
-#print(base_url)
 
 
 print("Welcome into the Weather app")
 print("please search for a country ")
 
 
-
- 
 def location_input():
-   
+
     city = input('Enter the name of the city.. >  ')
-    res = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no")
-    print ("Searching..\n")
-    print(res.json())
+
+    try:
+        res = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no")
+        print ("Searching..\n")
+       
+        parse_json = json.loads(res.text)
+        weather_status = json.dumps(parse_json["current"]["condition"].get("text"))
+
+        localtime = json.dumps(parse_json["location"].get("localtime"), indent=4)
+        temprature = json.dumps(parse_json["current"].get("temp_c"), indent=4)
+        
     
-    
+        print("Weather Status: " + weather_status)
+        print("Localtime is " + localtime  + " and " +  " the temprature (celcius) is "  + temprature)
+    except:
+      print("Exception thrown. x does not exist.")
+
 location_input()
 
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
 
