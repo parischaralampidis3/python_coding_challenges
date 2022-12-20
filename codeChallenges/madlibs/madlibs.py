e782bd71e15e119ac452b2358c6be235ee124e87
Paris Charalampidis <curlycode@outlook.com>
2022-12-20 12:58:46 +0200

    setting up functional logic for the game

--- codeChallenges/madlibs/madlibs.py
+++ codeChallenges/madlibs/madlibs.py
@@ -1,26 +1,70 @@
-import re
+
 
 def introduction():
     print ("Welcome at the madlibs game!\n")
-    print("Please fill the gaps with the right choise.\n")
-    print("Are you ready to continue?\n")
-    print("Type y/Y for yes, or n/N for no, to continue..")
+    print("Please fill the gaps, while type the right number of the choise.\n")
 introduction()
 
-def input_choice():
+def enter_stage():
+     print("Your challenge comes.")
+     print("type the number of the correct choice.") 
+enter_stage()
+
+def first_stage(attr1,attr2,attr3):   
+    print("Little lamp, sweet and fat, his mothers only _____\n")
+    print("1 [proud]\n")
+    print("2 [count \n")
+    print("3 [pound]\n")
+
+    first_stage = input ("> ")
+
+    if first_stage == attr1:
+        print ("Correct!")
+    elif first_stage == attr2 or first_stage == attr3:
+        print("not correct")
+    else:
+        print("sorry i don't understand")
+first_stage("1","2","3")
+
+def input_choice(attr1,attr2,attr3,attr4):    
+    print("Are you ready to continue?\n")
+    print("Type y/Y for yes, or n/N for no, to continue..")
     decision = input("> ")
     print(decision)
-    if decision == 'Yes' or decision == 'yes':
-         print("It was yes")
-    elif decision == 'No' or decision == 'no':
+    if decision == attr1 or decision == attr2:
+         print("Please continue..")
+    elif decision == attr3 or decision == attr4:
         print("no")
+        exit()
     else:
-        print("sorry i dont understand")
+        print("sorry i dont understand..pleace try again..")
+input_choice("Yes","yes","No","no")
 
-   
+def first_stage(attr1,attr2,attr3):   
+    enter_stage()
+    print("Little lamp, sweet and fat, his mothers only _____\n")
+    print("1 [proud]\n")
+    print("2 [count \n")
+    print("3 [pound]\n")
 
-input_choice()
-   
+    first_stage = input ("> ")
+    if first_stage == attr1:
+        print ("Correct!")
+    elif first_stage == attr2 or first_stage == attr3:
+        print("not correct")
+
+first_stage("1","2","3")
+input_choice("Yes","yes","No","no")
+
+
+
+
+
+
+
+
+
+
 
 
    
---
+++ codeChallenges/madlibs/test.py
@@ -0,0 +1,19 @@
+
+
+
+""" 
+def divide (div):
+    d2 = div/2
+    print(d2)
+    return d2
+divide(10)
+ 
+def sum (num):
+    s1 = divide(num)
+    s2 = s1 + 6
+    print(s2)
+    return s2
+ 
+num = 12
+"""
+
