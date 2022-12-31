84b40c82188d8a43c23c074f903184bb5d9007ec
Paris Charalampidis <curlycode@outlook.com>
2022-12-31 02:34:44 +0200

    set start functionality for a timer

---
+++ codeChallenges/count_down timer/index.py
@@ -0,0 +1,18 @@
+#import time modules
+import time as t
+
+
+def set_timer():
+    print("set preffered seconds.. ")
+    seconds =int(input('> '))
+    print ("preffered seconds: %s " % seconds)
+ 
+#LOOP THROUGH  NUMBERS 
+    for i in range(seconds):
+        print(str(seconds-i),end='\r')
+        t.sleep(2)
+set_timer()
+
+
+
+
