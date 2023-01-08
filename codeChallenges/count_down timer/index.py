6e49c4aa57130347792d41018e559509e3771e1b
Paris Charalampidis <curlycode@outlook.com>
2023-01-08 21:02:23 +0200

    changes made

--- codeChallenges/count_down timer/index.py
+++ codeChallenges/count_down timer/index.py
@@ -1,18 +1,21 @@
 #import time modules
 import time as t
 
-
 def set_timer():
     print("set preffered seconds.. ")
     seconds =int(input('> '))
     print ("preffered seconds: %s " % seconds)
- 
+    
+    print("Your timer is going to begin..Ready?")
+    t.sleep(3)
 #LOOP THROUGH  NUMBERS 
+
     for i in range(seconds):
-        print(str(seconds-i),end='\r')
+        print("remaining seconds: " +str(seconds-i ),end='\n \r')
         t.sleep(2)
 set_timer()
 
+print("Happy new Year!")
 
 
 
